b5b12bfb5a1d1c15a1fbd45fb695220bc6341f8f
WildanFahri <Wildan.fahri03@gmail.com>
2021-04-14 14:20:07 +0700

    edit migration matakuliah

--- database/migrations/2021_04_14_070909_create_matakuliah_table.php
+++ database/migrations/2021_04_14_070909_create_matakuliah_table.php
@@ -13,9 +13,14 @@ class CreateMatakuliahTable extends Migration
      */
     public function up()
     {
-        Schema::create('matakuliah', function (Blueprint $table) {
+        Schema::create('mahasiswa_matakuliah', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->string('mahasiswa_nim', 20)->nullable();
+            $table->unsignedBigInteger('matakuliah_id')->nullable();
+            $table->foreign('mahasiswa_nim')->references('nim')->on('mahasiswa');
+            $table->foreign('matakuliah_id')->references('id')->on('matakuliah');
+            $table->integer('nilai');
         });
     }
 
@@ -26,6 +31,6 @@ class CreateMatakuliahTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('matakuliah');
+        Schema::dropIfExists('matakuliah_matakuliah');
     }
 }
